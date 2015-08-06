9b369584406843d836298b76d760ba6aa772e7db
MichaelGofron <gofronm@yahoo.com>
2015-08-06 15:22:34 -0700

    Added routing for the next page (question_display)

--- app/create/createCtrl.js
+++ app/create/createCtrl.js
@@ -3,12 +3,13 @@
 // Display all users current joined
 // Contains a go button that will navigate the user to the next page, and set the join flag to false
 angular.module("App")
-.controller("createCtrl", [$scope, $state, "firebaseFactory", function($scope, $state, firebaseFactory){
+.controller("createCtrl", ["$scope", "$state", "firebaseFactory", function($scope, $state, firebaseFactory){
 
   // get the game code to make sure it works
   var game = firebaseFactory.getGame();
   console.log(game);
-  $scope.code = game.key();
+  // $scope.code = game.key();
+  $scope.code = "Test";
 
   // must get the number of players to display
   $scope.players = game.players;
@@ -16,5 +17,7 @@ angular.module("App")
   $scope.toQuestionDisplay = function(){
     $state.go("question_display");
   };
+
+  console.log("working");
 
 }]);
