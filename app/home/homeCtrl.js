a6ce92635f28667c476c7c188845e34a7e28e8e7
Dave Schumaker <daveschumaker@gmail.com>
2015-08-06 16:30:52 -0700

    Remove automagically created player 'Dave' each time Home Page loads.

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -1,6 +1,6 @@
 angular.module("App")
 .controller("homeCtrl", function($scope, $state, fireBaseFactory){
-  $scope.join = fireBaseFactory.joinGame("-Jw3HM066k7KPSn1RgKN", "Dave");
+  // $scope.join = fireBaseFactory.joinGame("-Jw3HM066k7KPSn1RgKN", "Dave");
   // $scope.text = fireBaseFactory.createGame();
   $scope.routing = function(){
     fireBaseFactory.create();
