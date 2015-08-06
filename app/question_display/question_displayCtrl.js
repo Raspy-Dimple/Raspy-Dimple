e55a121bc351340e783b740ccd003893f6f68b85
lisatsewang <lisatsewang@gmail.com>
2015-08-06 16:45:00 -0700

    able to display a question from a hard coded questions object, add next button

--- app/question_display/question_displayCtrl.js
+++ app/question_display/question_displayCtrl.js
@@ -1,6 +1,22 @@
-app.module("App")
+angular.module("App")
 .controller("question_displayCtrl", function($scope, $state, fireBaseFactory) {
 	// get question from firebase to display question
-	$scope.question = fireBaseFactory.getGame()
+	// var game = fireBaseFactory.getGame();
+	// var questions = game.questions;
+	var timer = 30;
+
+	setInterval(function() {
+		$scope.sec = timer;
+		timer--;
+	}, 1000);
+
+	var questions = {0: "question1", 1: "question2"};
+
+	var questionIndex = 0;
+	$scope.question = questions[questionIndex];
+	questionIndex++;
+
+
+
 	// countdown 30 sec
 })
