21131fb3c6dd51903102a43416b46c471facc9a3
Dave Schumaker <daveschumaker@gmail.com>
2015-08-07 15:16:47 -0700

    Player results view now works and redirects players to next question.

--- app/result_player/result_playerCtrl.js
+++ app/result_player/result_playerCtrl.js
@@ -2,10 +2,20 @@ angular.module('App')
   .controller('result_playerCtrl', function($scope, fireBaseFactory) {
     var game = fireBaseFactory.getGame();
     var playerKey = fireBaseFactory.getPlayerKey();
+    var playerList = {}; // Store our player list.
     
     game.$loaded()
       .then(function(data) {
         $scope.question = data.questions[data.currentRound];
         $scope.answers = data.answers;
+        //$scope.playerNames = data.players.
+        console.log('DATA.PLAYERS ', data.players);
+        playerList = data.players;
       });
+
+    $scope.getPlayerName = function(playerKey) {
+      console.log('Player Key Object: ', playerList[playerKey]);
+      return playerList[playerKey].name;
+    }
+
   })
