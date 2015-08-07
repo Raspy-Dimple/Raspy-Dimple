9e3a2d3f83d13fef7230803c02fa806e1e18da44
lisatsewang <lisatsewang@gmail.com>
2015-08-06 17:14:45 -0700

    adjust stuff

--- app/question_display/question_displayCtrl.js
+++ app/question_display/question_displayCtrl.js
@@ -1,14 +1,9 @@
 angular.module("App")
 .controller("question_displayCtrl", function($scope, $state, fireBaseFactory) {
+	
 	// get question from firebase to display question
 	// var game = fireBaseFactory.getGame();
 	// var questions = game.questions;
-	var timer = 30;
-
-	setInterval(function() {
-		$scope.sec = timer;
-		timer--;
-	}, 1000);
 
 	var questions = {0: "question1", 1: "question2"};
 
@@ -16,7 +11,8 @@ angular.module("App")
 	$scope.question = questions[questionIndex];
 	questionIndex++;
 
+	$scope.toVotingDisplay = function() {
+		$state.go("voting_display");
+	};
 
-
-	// countdown 30 sec
 })
