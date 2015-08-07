6921708211a8993e0b45db064115528c2148e3b0
jplourd <jeffplourd@gmail.com>
2015-08-06 20:34:09 -0700

    Completes question player. Player's can respond with answers.

--- app/question_player/question_playerCtrl.js
+++ app/question_player/question_playerCtrl.js
@@ -2,17 +2,22 @@ angular.module('App')
   .controller('question_playerCtrl', function($scope, fireBaseFactory) {
     var game = fireBaseFactory.getGame();
     var playerKey = fireBaseFactory.getPlayerKey();
-    var currentRound = game.currentRound;
+    var playerAnswer = null;
+    // var currentRound = game.currentRound;
     
     // question_player will need to display the current question to the player
-    $scope.question = game.questions[currentRound];
+    // $scope.question = game.questions[currentRound];
+    game.$loaded()
+      .then(function(data) {
+        $scope.question = data.questions[data.currentRound];
+      });
 
     // question_player will need to take the player's answer and add it to the
     // database
     $scope.submitPlayerAnswer = function(answer) {
-      // check if answer
-      if(game.playerAnswers === null) {
-        
-      }
+      console.log(answer);
+      console.log(game);
+      var ref = new Firebase('https://exposeyourself.firebaseio.com/games/' + game.$id);
+      ref.child('answers').child(playerKey).update({response: answer});
     };
   });
