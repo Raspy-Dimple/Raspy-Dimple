a9e14a8d8fc171d143fe151bcd3f26f066131090
MichaelGofron <gofronm@yahoo.com>
2015-08-10 12:39:28 -0700

    Some things done for stopping answer

--- app/voting_player/voting_playerCtrl.js
+++ app/voting_player/voting_playerCtrl.js
@@ -6,14 +6,28 @@ angular.module('App')
     
     game.$loaded()
       .then(function(data) {
+        // if question happens to be written by the same person don't set the $scope.question to that element
+        // maybe remove it
         $scope.question = data.questions[data.currentRound];
         $scope.currentRound = data.currentRound;
-        // $scope.answers = fireBaseFactory.getPlayerAnswers();
+        console.log("Answers",data.answers);
+        $scope.answers = data.answers;
+        $scope.answers = removePlayerAnswer(data.answers);
       });
 
     $scope.chooseAnswer = function(playerKey) {
       // increment the current answers vote count
       fireBaseFactory.incrementPlayerScore(playerKey);
       $state.go('result_player');
+    };
+
+    var removePlayerAnswer = function(answers){
+      var newAnswers = [];
+      for (answer in answers){
+        if (answer[playerKey] !== playerKey){
+          newAnswers.push(answer);
+        }
+      }
+      return newAnswers;
     };
   });
