19e7fd222356a2838cf70067da8d68c89712beba
MichaelGofron <gofronm@yahoo.com>
2015-08-10 17:12:59 -0700

    save changes temporarily

--- app/voting_player/voting_playerCtrl.js
+++ app/voting_player/voting_playerCtrl.js
@@ -16,9 +16,8 @@ angular.module('App')
         // maybe remove it
         $scope.question = data.questions[data.currentRound];
         $scope.currentRound = data.currentRound;
-        console.log("Answers",data.answers);
         $scope.answers = data.answers;
-        $scope.answers = removePlayerAnswer(data.answers);
+        // $scope.answers = removePlayerAnswer(data.answers);
       });
 
     $scope.chooseAnswer = function(playerKey) {
@@ -39,13 +38,15 @@ angular.module('App')
       },500,0);
     };
 
-    var removePlayerAnswer = function(answers){
-      var newAnswers = [];
-      for (answer in answers){
-        if (answer[playerKey] !== playerKey){
-          newAnswers.push(answer);
-        }
-      }
-      return newAnswers;
-    };
+    // var removePlayerAnswer = function(answers){
+    //   var newAnswers = [];
+    //   console.log("removePlayer",answers);
+    //   for (var key in answers){
+    //     if (key !== playerKey){
+    //       newAnswers.push(answers[key]);
+    //     }
+    //   }
+    //   return newAnswers;
+    // };
+
   });
